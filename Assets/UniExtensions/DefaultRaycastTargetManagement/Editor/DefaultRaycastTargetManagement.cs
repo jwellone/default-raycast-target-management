5a368f96f87f859784fdabc47388d363f7d15dbf
macbookpro-yusui <yasuhiko-usui@kandycodings.jp>
2021-07-31 11:56:36 +0900

    ・Unityプロジェクト追加 ・gitignore編集 ・README.md編集

---
+++ Assets/UniExtensions/DefaultRaycastTargetManagement/Editor/DefaultRaycastTargetManagement.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections.Generic;
+using UnityEditor;
+using UnityEditor.Experimental.SceneManagement;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+namespace UniExtensions.Editor
+{
+	[InitializeOnLoadAttribute]
+	public static class DefaultRaycastTargetManagement
+	{
+		private const float PREFAB_EDIT_MODE_INTERVAL = 0.15f;
+		private static long s_hashSum = 0L;
+		private static float s_prefabEditModeUpdateInterval;
+		private static HashSet<Graphic> s_graphics = new HashSet<Graphic>();
+
+		static DefaultRaycastTargetManagement()
+		{
+			EditorApplication.hierarchyChanged += OnHierarchyChanged;
+			EditorApplication.update += OnPrefabStageUpdate;
+			EditorApplication.playModeStateChanged += OnPlayModeStateChanged;
+		}
+
+		private static void OnHierarchyChanged()
+		{
+			if (Application.isPlaying)
+			{
+				return;
+			}
+
+			if (PrefabStageUtility.GetCurrentPrefabStage() != null)
+			{
+				return;
+			}
+
+			var validScenes = new List<Scene>(SceneManager.sceneCount);
+			for (var i = 0; i < SceneManager.sceneCount; ++i)
+			{
+				var scene = SceneManager.GetSceneAt(i);
+				if (scene.isLoaded && scene.IsValid())
+				{
+					validScenes.Add(scene);
+				}
+			}
+
+			var sum = 0L;
+			foreach (var scene in validScenes)
+			{
+				sum += scene.GetHashCode();
+			}
+
+			if (sum != s_hashSum)
+			{
+				s_hashSum = sum;
+				s_graphics.Clear();
+				foreach (var scene in validScenes)
+				{
+					foreach (var obj in scene.GetRootGameObjects())
+					{
+						var graphics = obj.GetComponentsInChildren<Graphic>();
+						foreach (var graphic in graphics)
+						{
+							s_graphics.Add(graphic);
+						}
+					}
+				}
+				return;
+			}
+
+			foreach (var scene in validScenes)
+			{
+				foreach (var obj in scene.GetRootGameObjects())
+				{
+					AddNewGraphics(obj.GetComponentsInChildren<Graphic>());
+				}
+			}
+		}
+
+		private static void OnPrefabStageUpdate()
+		{
+			if (Application.isPlaying)
+			{
+				return;
+			}
+
+			s_prefabEditModeUpdateInterval -= Time.fixedUnscaledDeltaTime;
+			if (s_prefabEditModeUpdateInterval > 0f)
+			{
+				return;
+			}
+			s_prefabEditModeUpdateInterval = PREFAB_EDIT_MODE_INTERVAL;
+
+			var prefabStage = PrefabStageUtility.GetCurrentPrefabStage();
+			if (prefabStage == null)
+			{
+				return;
+			}
+
+			var hashCode = prefabStage.GetHashCode();
+			if (hashCode != s_hashSum)
+			{
+				s_hashSum = hashCode;
+				s_graphics.Clear();
+
+				var graphics = prefabStage.prefabContentsRoot.GetComponentsInChildren<Graphic>();
+				foreach (var graphic in graphics)
+				{
+					s_graphics.Add(graphic);
+				}
+				return;
+			}
+
+			AddNewGraphics(prefabStage.prefabContentsRoot.GetComponentsInChildren<Graphic>());
+		}
+
+		private static void OnPlayModeStateChanged(PlayModeStateChange state)
+		{
+			if (state == PlayModeStateChange.EnteredPlayMode)
+			{
+				Clear();
+			}
+		}
+
+		private static void Clear()
+		{
+			s_graphics.Clear();
+			s_hashSum = 0L;
+			s_prefabEditModeUpdateInterval = 0f;
+		}
+
+		private static void AddNewGraphics(in Graphic[] graphics)
+		{
+			foreach (var graphic in graphics)
+			{
+				if (s_graphics.Contains(graphic))
+				{
+					continue;
+				}
+
+				graphic.raycastTarget = GetRaycastTarget(graphic);
+				s_graphics.Add(graphic);
+			}
+		}
+
+		private static bool GetRaycastTarget(in Graphic graphic)
+		{
+			if (graphic.GetComponent<IEventSystemHandler>() != null)
+			{
+				return true;
+			}
+
+			var parent = graphic.transform.parent?.gameObject;
+			if (parent == null)
+			{
+				return false;
+			}
+
+			if (parent.GetComponent<Slider>() != null || parent.GetComponent<Toggle>() != null)
+			{
+				return true;
+			}
+
+			return false;
+		}
+	}
+}
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# default-raycast-target-management
+# default-raycast-target-management(ver1.0.0)
+Of the newly added ugui components
+If you don't need the raycast target, uncheck it.
+
+# GET STARTED
+It can be used by importing the package.
+
+# Author Info
+Created by Yasuhiko Usui
+
+facebook: https://www.facebook.com/Jwellone-Inc-110584057879884/
+
+# License
+This extension is under the [MIT License](https://github.com/jwellone/file-size-viewer/blob/main/LICENSE).
